9c43c42935fe356671d29a87dd9442e71b345451
sergioandresco <sergiocobos6@outlook.com>
2022-10-11 16:08:49 -0500

    Commit 9

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+(function(){
+    const oppenButton = document.querySelector('.nav__menu');
+    const menu = document.querySelector('.nav__link');
+    const closeMenu = document.querySelector('.nav__close');
+
+    oppenButton.addEventListener('click', () => {
+        menu.classList.add('nav__link--show');
+    });
+
+    closeMenu.addEventListener('click', () => {
+        menu.classList.remove('nav__link--show');
+    });
+})();

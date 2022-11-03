bbf7ea36646142e5b04fd7d623acec68dd6079ee
Sergio <43445037+sergioandresco@users.noreply.github.com>
2022-11-03 12:00:38 -0500

    Commit Sergio Prueba 2

--- Index.php
+++ Index.php
@@ -16,7 +16,7 @@
     <header class="hero">
         <nav class="nav container">
             <div class="nav__logo">
-                <h1>ESTO ES UNA PRUEBAAAAAAAA</h1>
+                <h1>Prueba numero 2</h1>
                 <h2 class="nav__title">SERCHTEC
                     <img src="./images/glasses.svg" class="icon__logo">
 
